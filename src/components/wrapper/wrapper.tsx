21be64b9cbcb61602eefcee11c09ec3fc66225b9
Harao <haraodev@gmail.com>
2026-01-12 18:21:17 -0300

    hotfix: Nome errado

--- src/components/wrapper/wrapper.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from "react";
-
-const Wrapper: React.FC<{ children: ReactNode }> = ({ children }) => {
-  return <div className="container mx-auto py-5 flex flex-col md:flex-row  gap-5">{children}</div>;
-};
-
-export default Wrapper;
